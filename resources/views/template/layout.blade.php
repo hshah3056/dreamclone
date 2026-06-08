fa2ceb7fc5f248fd7b921bdbf7343fb572f50c27
Harsh <dreamclone2501@gmail.com>
2026-06-08 10:24:36 +0000

    minor changes

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -31,14 +31,14 @@
         <meta name="twitter:image" content="{{ $image }}">
         <meta name="theme-color" content="#ffffff">
 
-        <link rel="icon" type="image/x-icon" href="favicon.ico">
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon/logo-16x16.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/logo-16x16.png">
+        <link rel="icon" type="image/x-icon" href="public/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="assets/img/logo-16x16.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logo-16x16.png">
 
-        <link rel="icon" type="image/x-icon" href="assets/img/favicon/logo-32x32.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/logo-32x32.png">
+        <link rel="icon" type="image/x-icon" href="assets/img/logo-32x32.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/logo-32x32.png">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favicon/logo-180x180.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/logo-180x180.png">
         <meta name="apple-mobile-web-app-title" content="Dreamclone">
 
         <link rel="canonical" href="{{ $url }}">
